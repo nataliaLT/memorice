27b97454723bcc66e93bce6f73ad6f80ea3137f1
natalia <natalia.lucerot@gmail.com>
2018-10-21 17:27:57 -0500

    Updates

---
+++ precache-manifest.ef2462d300c23f344d39026470a4e246.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memorice/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "26d3d19e2daa6e51ed91",
+    "url": "/memorice/static/js/runtime~main.26d3d19e.js"
+  },
+  {
+    "revision": "bd60ec2a2f4c732d9b83",
+    "url": "/memorice/static/js/main.bd60ec2a.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/memorice/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "bd60ec2a2f4c732d9b83",
+    "url": "/memorice/static/css/main.39d6cffc.chunk.css"
+  },
+  {
+    "revision": "8f3c9cd2f731cd11970c5844cc27f95c",
+    "url": "/memorice/index.html"
+  }
+];
